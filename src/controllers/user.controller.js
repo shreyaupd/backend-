8490672760fad1777649a754172fcc8e7a9902f6
shreyaupd <shreyaupd234@gmail.com>
2025-03-27 20:10:14 +0545

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,20 +1,32 @@
 //Routes are the endpoints that define how the server will respond to client requests. They are the URLs that clients use to interact with the server. Routes are defined in the Express application and are used to perform different actions based on the client's request.Controllers in express are used to handle the incoming requests and responses. They are the middlemen between the routes and the database. They contain the logic of the application.
 
 
-import {asyncHandeller} from "../utils/asyncHandeller.js" 
+import asyncHandeller from "../utils/asyncHandeller.js";
 
-//method
+const registerUser = asyncHandeller(async (req, res) => {
 
-const registerUser = asyncHandeller(async(req ,res)=>{
- //https status code 200 is success code that means it is ok (status code 100 is informational, 200 is success, 300 is redirection, 400 is client error, 500 is server error)
+//1.get user details from frontend
+//2.validate if empty or not
+//3.check if user already exists via email and username
+//4.check avatar and image
+//5.upload cloudinary
+//6.create user object 
+//7.remove password and refresh token from response
+//8.check for user validation
+//9.return res (response) to frontend
 
-    res.status(200).json({ 
-        message:"ok"
-    }) ////the JSON response that is sent when resisterUser is executed. eg Sends { "message": "ok" } with status 201 (Created).
+//1. get user details from frontend
+const { username, email, fullname, password } = req.body;
+console.log("email", email);
+res.status(200).json({ message: "User registered successfully" });
 })
-
 // note:
 // Uses asyncHandeller to catch errors automatically.
 // Handles an HTTP request.
 // Responds with a 200 OK status and { message: "ok" } as JSON.
+
 export default registerUser;
+
+//Routes are the endpoints that define how the server will respond to client requests. They are the URLs that clients use to interact with the server. Routes are defined in the Express application and are used to perform different actions based on the client's request.Controllers in express are used to handle the incoming requests and responses. They are the middlemen between the routes and the database. They contain the logic of the application.
+ 
+
